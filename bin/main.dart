780db40604cb5b3e764f25be04dcca763bfa7fec
Milan Koirala <koiralamilon67@gmail.com>
2025-06-18 16:42:34 +0545

    Initial commit - Dart E-Commerce CLI project

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:ecommerce_cli/ecommerce_cli.dart' as ecommerce_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ecommerce_cli.calculate()}!');
+}
---
+++ test/ecommerce_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:ecommerce_cli/ecommerce_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
