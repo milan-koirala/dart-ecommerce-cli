c52f7f30517d59d3356535dc9b7ed88b9853d822
Milan Koirala <koiralamilon67@gmail.com>
2025-06-20 23:51:38 +0545

    update product variables

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -3,29 +3,39 @@ class Product {
   String name;
   double price;
   int stock;
+  String category;
 
   Product({
     required this.id,
     required this.name,
     required this.price,
     required this.stock,
+    this.category = '',
   });
 
   factory Product.fromJson(Map<String, dynamic> json) {
     return Product(
-      id: json['id'],
-      name: json['name'],
-      price: json['price'],
-      stock: json['stock'],
+      id: json['id'] as String,
+      name: json['name'] as String,
+      price: (json['price'] as num).toDouble(),
+      stock: json['stock'] as int,
+      category: json['category'] as String? ?? '',
     );
   }
 
   Map<String, dynamic> toJson() {
-    return {'id': id, 'name': name, 'price': price, 'stock': stock};
+    return {
+      'id': id,
+      'name': name,
+      'price': price,
+      'stock': stock,
+      'category': category,
+    };
   }
 
   @override
   String toString() {
-    return '🛒 $name | 💲$price | Stock: $stock';
+    final categoryDisplay = category.isNotEmpty ? ' | Category: $category' : '';
+    return '$name | 💲$price | Stock: $stock$categoryDisplay';
   }
 }
